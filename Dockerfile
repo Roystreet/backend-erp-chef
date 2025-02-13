a2f6535bc00145a441bc61a52791311cdbd6edc3
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2025-02-13 11:52:45 -0400

    feature: create controller

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+Article, Category, Customer, CustomerPurchase, CustomerType,
+Franchise, FranchiseArticle, PriceHistory, Role, Stock,
+StockMovement, Supplier, SupplierOrder, Unit, User, UserRole
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+
 abstract class Controller
 {
-    //
+
+    public function getArticle($id)
+    {
+        return Article::find($id);
+    }
+
+    public function getAllArticles()
+    {
+        return Article::all();
+    }
+
+    public function getFilterArticles($filter)
+    {
+        return Article::where('name', 'like', '%' . $filter . '%')->get();
+    }
 }
